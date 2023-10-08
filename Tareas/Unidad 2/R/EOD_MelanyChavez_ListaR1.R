3c79be5298ac68ff0cbe7959cc7a0f8ceb0c12d3
Mely-Chavez <L211050227@pabellon.tecnm.mx>
2023-10-07 23:09:16 -0600

    Capturas y actividad

---
+++ Tareas/Unidad 2/R/EOD_MelanyChavez_ListaR1.R
@@ -0,0 +1,32 @@
+ITIC3 <- list("Reiner Rivera", "juan Prieto","Melany Chavez", "Vane Chavez");
+Edades <- list(18,18,20,19);
+Carreras <- list("ITIC", "Logistica", "Gestion Empresarial", "Mecatronita", "Industrial");
+EdadesAlumnos <- list(ITIC3, Edades);
+View(EdadesAlumnos)
+EdadesAlumnos[1]
+ITIC3[2]
+EdadesAlumnos[1][1]
+EdadesAlumnos[1][1][1]
+View(Edades)
+length(Edades)
+length(EdadesAlumnos)
+Edades[4] <- ("32");
+Edades <- ("30");
+Edades[5] <- list(25);
+Edades <- list(18,19,20,21,30);
+Edades[5] <- list(25)
+View(Edades)
+Edades
+
+ITIC3[5] <- list("Vane")
+View(ITIC3)
+ITIC3
+
+ITIC3[1] <- NULL
+View(ITIC3)
+ITIC3
+
+sort(Edades)
+Edades2 <- list(17,20,21);
+View(Edades)
+Edades2
