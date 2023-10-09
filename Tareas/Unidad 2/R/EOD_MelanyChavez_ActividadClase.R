25fc5324f6e9087f434dbd929b28ab1b5ac2a437
Mely-Chavez <L211050227@pabellon.tecnm.mx>
2023-10-09 10:01:37 -0600

    09/10/23

---
+++ Tareas/Unidad 2/R/EOD_MelanyChavez_ActividadClase.R
@@ -0,0 +1,16 @@
+library(readr)
+Localidades <- read_csv("C:/Users/COMPUTOCKS/Pictures/ESTRUCTURA DE DATOS/ESTRUCTURA_DE_DATOS2/Tareas/Unidad 2/R/Copia de Catalogo de Localidades.csv")
+View(Localidades)
+library(readr)
+Municipio <- read_csv("C:/Users/COMPUTOCKS/Pictures/ESTRUCTURA DE DATOS/ESTRUCTURA_DE_DATOS2/Tareas/Unidad 2/R/Copia de Catalogo de Municipios.csv")
+View(Municipio)
+View (Localidades)
+Estados <- list(Localidades)
+View(Estados)
+Estados[1]
+length(Estados)
+length(Localidades)
+lengths(Localidades)
+view (Municipio)
+Municipio [PABELLON DE ARTEAGA] <- NULL
+Municipio [6] <- NULL
